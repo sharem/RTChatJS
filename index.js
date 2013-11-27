0c2a91f95e644e5edf9da08fa5504ef5c07f7160
Iraide <iraide.diaz@gmail.com>
2013-11-27 16:13:10 +0100

    Basic backend finished.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var io = require('socket.io');
 var app = express();
 var port = 3700;
  
@@ -12,5 +11,14 @@ app.get("/", function(req, res){
 
 app.use(express.static(__dirname + '/public'));
 
-io.listen(app.listen(port));
+
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
 console.log("Listening on port " + port);
