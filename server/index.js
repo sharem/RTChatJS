aa159cc3a27e0d71a445c79f5e6864be8c410387
Iraide Diaz <iraide.diaz@gmail.com>
2026-04-28 15:01:04 +0200

    chore: update package.json and package-lock.json to specify Node and npm engine versions

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const path = require('path');
+
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
